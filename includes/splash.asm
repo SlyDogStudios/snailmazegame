769725a1ab40aa432dee2be34a55afb53978bb85
CLIK-CENTRAL\Rob <slydogstudios@gmail.com>
2020-01-01 11:39:28 -0600

    Changed splash screen

--- includes/splash.asm
+++ includes/splash.asm
@@ -1,91 +1,18 @@
-logo1:
-	.byte $dc,$dd,$de,$df,$e0,$e1
-logo2:
-	.byte $e2,$e3,$e4,$e5,$e6,$e7
-logo3:
-	.byte $e8,$e9,$ea,$eb,$ec,$ed
-logo4:
-	.byte $ee,$ef,$f0,$f1,$f2,$f3
-logo5:
-	.byte $f4,$f5,$f6,$f7,$f8,$f9
-logo6:
-	.byte $fa,$fb,$fc,$fd,$fe,$ff
 
-slydogstudios:
-;	.byte "SLY DOG STUDIOS (2012)"
-	.byte $80,$81,$82,$20,$83,$84,$85,$20,$80,$86,$87,$83,$88,$84,$80,$20,$8c,$89,$8a,$8b,$89,$8d
+robertlbryant:
+;	.byte "ROBERT L BRYANT (2012)"
+	.byte $8f,$84,$d3,$d5,$8f,$86,$20,$81,$20,$d3,$8f,$82,$8e,$7f,$86,$20,$8c,$89,$8a,$8b,$89,$8d
 
 splash_table:
 	.byte $00,$02,$04,$06,$08,$0a,$0c,$0e,$10,$12,$14,$16,$18,$1a,$1c,$1e
 
 the_splash:
-	lda #$22
-	sta $2006
-	lda #$c2
-	sta $2006
-	ldx #$00
-:	lda logo1, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
-	lda #$22
-	sta $2006
-	lda #$e2
-	sta $2006
-	ldx #$00
-:	lda logo2, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
 	lda #$23
 	sta $2006
-	lda #$02
+	lda #$45
 	sta $2006
 	ldx #$00
-:	lda logo3, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
-	lda #$23
-	sta $2006
-	lda #$22
-	sta $2006
-	ldx #$00
-:	lda logo4, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
-	lda #$23
-	sta $2006
-	lda #$42
-	sta $2006
-	ldx #$00
-:	lda logo5, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
-	lda #$23
-	sta $2006
-	lda #$62
-	sta $2006
-	ldx #$00
-:	lda logo6, x
-	sta $2007
-	inx
-	cpx #$06
-	bne :-
-
-	lda #$23
-	sta $2006
-	lda #$69
-	sta $2006
-	ldx #$00
-:	lda slydogstudios, x
+:	lda robertlbryant, x
 	sta $2007
 	inx
 	cpx #$16
